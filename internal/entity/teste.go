4dcd31196d682e02e009dcd2a7d22ea09366de95
Higor <higor.vinicius@flashcover.com.br>
2025-12-10 08:14:50 -0300

    Atualizando informações de logica para criar relatorio dto

--- internal/entity/teste.go
+++ internal/entity/teste.go
@@ -1,39 +1,71 @@
 package entity
 
+import (
+	"context"
+	"sync"
+
+	"github.com/Higor-ViniciusDev/stress_cli/internal/service"
+)
+
 type TesteRequest struct {
 	Url       string
 	Requests  int
 	Conc      int
-	InputChan chan string
+	InputChan chan any
 }
 
 type FilaRequest struct {
-	Request chan int
-	Cont    int
-	Url     string
+	Cont int
+	mu   sync.Mutex
 }
 
 func NovoTeste(url string, intRequests int, intConc int) *TesteRequest {
 	teste := &TesteRequest{
-		Url:      url,
-		Requests: intRequests,
-		Conc:     intConc,
+		Url:       url,
+		Requests:  intRequests,
+		Conc:      intConc,
+		InputChan: make(chan any, intRequests),
 	}
 
 	return teste
 }
 
-func (t *TesteRequest) Executar() {
+func (t *TesteRequest) Executar() chan any {
 	novoFilaRequest := &FilaRequest{
-		// Request: make(int, t.re)
+		Cont: t.Requests,
 	}
 
-	for range t.Requests {
-		t.InputChan <- t.Url
+	for {
+		if novoFilaRequest.Cont == 0 {
+			break
+		}
+
+		timeStarted := make(chan string)
+
+		for range t.Conc {
+			go t.Processar(t.Url, timeStarted, novoFilaRequest)
+		}
+
+		close(timeStarted)
 	}
+
+	return t.InputChan
 }
 
-func (t *TesteRequest) Processar(url string, timeStarted chan (string)) {
+func (t *TesteRequest) Processar(url string, timeStarted chan (string), fila *FilaRequest) {
+	novoService := &service.ServiceRequest{}
+	ctx := context.Background()
+
+	if fila.Cont == 0 {
+		return
+	}
+
+	fila.mu.Lock()
+	fila.Cont--
+	fila.mu.Unlock()
 
 	<-timeStarted
+	retorno := novoService.FazerRequest(ctx, url)
+
+	t.InputChan <- retorno
 }
--- internal/usecase/create_usecase_teste.go
+++ internal/usecase/create_usecase_teste.go
@@ -9,18 +9,14 @@ func NewTesteUsecase() *TesteUsecase {
 	return &TesteUsecase{}
 }
 
-func (t *TesteUsecase) ExecutarTeste(url string, request, concurrency int) {
-	// Lógica para executar o teste de estresse
-	/*
-		Realizar requests HTTP para a URL especificada.
-		Distribuir os requests de acordo com o nível de concorrência definido.
-		Garantir que o número total de requests seja cumprido.
-		Geração de Relatório:
-	*/
+type RelatorioDTO struct {
+}
 
+func (t *TesteUsecase) ExecutarTeste(url string, request, concurrency int) *RelatorioDTO {
 	novoEntity := entity.NovoTeste(url, request, concurrency)
 
-	for range novoEntity.Executar() {
-		// Aqui você pode processar os resultados de cada request, se necessário
-	}
+	//Aqui processa retorno relatorio e preencher dto
+	novoEntity.Executar()
+
+	return &RelatorioDTO{}
 }
